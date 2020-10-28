e95e1b8cb6ebb64196384a27337c75eec9cb2678
Moritz <moritz.windmann@me.com>
2020-10-28 20:03:52 +0100

    Preload Typo

--- header.php
+++ header.php
@@ -19,6 +19,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 
 		<link rel="profile" href="https://gmpg.org/xfn/11">
+		<link rel="preload stylesheet" href="<?php echo get_template_directory_uri(); ?>/first.css">
 
 		<?php wp_head(); ?>
 
