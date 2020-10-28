2f925ae7930ad174aa2655ae194bca3c377a2c55
Moritz <moritz.windmann@me.com>
2020-10-28 16:59:27 +0100

    Structure

--- elements/list_card.php
+++ elements/list_card.php
@@ -1,3 +1,27 @@
-<!-- to be defined -->
+<?php
 
-<h4>Empty</h4>
+/**
+ * The default template for displaying content
+ *
+ * Used for both singular and index.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+?>
+
+<div class="list-item">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/400x300.png" alt="" />
+    <div class="content">
+        <h3 class="card-title">
+            Card Title
+        </h3>
+        <p class="preview-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+    </div>
+</div>
--- elements/square_card.php
+++ elements/square_card.php
@@ -1,3 +1,31 @@
-<!-- to be defined -->
+<?php
 
-<h4>Empty</h4>
+/**
+ * The default template for displaying content
+ *
+ * Used for both singular and index.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+?>
+
+
+<div class="card square shadow">
+    <a href="<?php echo esc_url( get_permalink() ); ?>">
+
+        <div class="content">
+            <h3 class="card-title">
+                Square Card Title
+            </h3>
+            <p class="preview-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+        </div>
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/400x400.png" alt="" />
+    </a>
+</div>
