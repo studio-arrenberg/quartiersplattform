4427606ee41e980c9ddd678fa31b5da8aa318931
j0hannr <johann.rohn@gmail.com>
2020-11-03 16:19:43 +0100

    image test

--- elements/card-projekte.php
+++ elements/card-projekte.php
@@ -22,7 +22,7 @@ $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "p
     <a href="<?php echo esc_url( get_permalink() ); ?>">
         <div class="content">
             <!-- <div class="pre-title">Pre-Title <span class="date">vor 30 Minuten<span></div> -->
-            <h3 class="card-title">
+            <h3 class="card-title"> HU..
                 <?php shorten_title(get_the_title(), '60'); ?>
             </h3>
             <p class="preview-text">
