aba5083a9dc8c0d2ebdf1651d32722a112bf3019
j0hannr <johann.rohn@gmail.com>
2020-11-04 13:29:25 +0100

    test

--- functions.php
+++ functions.php
@@ -78,7 +78,7 @@ function twentytwenty_theme_support() {
 		add_image_size( 'square_l', 310, 310, array( 'center', 'center' )); // this
 		// preview (4:3)
 		// add_image_size( 'preview_s', 160, 120);
-		// add_image_size( 'preview_m', 200, 150, array( 'center', 'center' )); // this
+		add_image_size( 'preview_m', 200, 150, array( 'center', 'center' )); // this
 		// add_image_size( 'preview_l', 800, 600, array( 'center', 'center' ));
 		// landscape (2:1)
 		// add_image_size( 'landscape_s', 200, 100); 
@@ -1201,3 +1201,70 @@ function my_deregister_styles() {
   wp_deregister_style( 'acf-input' );
   wp_deregister_style( 'acf-datepicker' );
 } add_action( 'wp_print_styles', 'my_deregister_styles', 100 );
+
+
+
+// dropdown button taxonomy
+// CTP anmerkungen TAX version
+function add_theme_box() {
+    add_meta_box('theme_box_ID', __('Version'), 'your_styling_function', 'anmerkungen', 'side', 'core');
+}   
+function add_theme_menus() {
+    if ( ! is_admin() )
+        return;
+    add_action('admin_menu', 'add_theme_box');
+    /* Use the save_post action to save new post data */
+    add_action('save_post', 'save_taxonomy_data');
+}
+add_theme_menus();
+// This function gets called in edit-form-advanced.php
+function your_styling_function($post) {
+    echo '<input type="hidden" name="taxonomy_noncename" id="taxonomy_noncename" value="' . wp_create_nonce( 'taxonomy_theme' ) . '" />';
+    // Get all theme taxonomy terms
+    $themes = get_terms('version', 'hide_empty=0'); 
+?>
+<select name='post_theme' id='post_theme'>
+    <!-- Display themes as options -->
+    <?php $names = wp_get_object_terms($post->ID, 'version'); ?>
+        <option class='theme-option' value=''<?php if (!count($names)) echo "selected";?>>None</option>
+        <?php
+		foreach ($themes as $theme) {
+			if (!is_wp_error($names) && !empty($names) && !strcmp($theme->slug, $names[0]->slug)) 
+				echo "<option class='theme-option' value='" . $theme->slug . "' selected>" . $theme->name . "</option>\n"; 
+			else
+				echo "<option class='theme-option' value='" . $theme->slug . "'>" . $theme->name . "</option>\n"; 
+		}
+   ?>
+</select>    
+<?php
+}
+
+function save_taxonomy_data($post_id) {
+// verify this came from our screen and with proper authorization.
+ 
+    if ( !wp_verify_nonce( $_POST['taxonomy_noncename'], 'taxonomy_theme' )) {
+        return $post_id;
+    }
+ 
+    // verify if this is an auto save routine. If it is our form has not been submitted, so we dont want to do anything
+    if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) 
+        return $post_id;
+ 
+    // Check permissions
+    if ( 'page' == $_POST['post_type'] ) {
+        if ( !current_user_can( 'edit_page', $post_id ) )
+            return $post_id;
+    } else {
+        if ( !current_user_can( 'edit_post', $post_id ) )
+        return $post_id;
+    }
+ 
+    // OK, we're authenticated: we need to find and save the data
+    $post = get_post($post_id);
+    if (($post->post_type == 'anmerkungen') || ($post->post_type == 'page')) { 
+           // OR $post->post_type != 'revision'
+           $theme = $_POST['post_theme'];
+       wp_set_object_terms( $post_id, $theme, 'version' );
+    }
+    return $theme;
+}
