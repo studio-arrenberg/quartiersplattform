04ca1881df7d5b72947d409e948cadd6c496bb93
Moritz <moritz.windmann@me.com>
2020-11-04 17:12:22 +0100

    Wellcome

---
+++ components/call-update.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Update
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+?>
+
+
+<div class="card landscape shadow lang shrink-desk">
+    <a href="<?php echo get_site_url(); ?>/arrenberg-update/">
+		<div class="content">
+			<!-- <h3 class="card-title">
+				Werde Mitglied in deinem Quartier
+			</h3>
+			<p class="preview-text">
+				+ Projekte erstellen <br>
+				+ Nachrichten veröffentlichen <br>
+				+ Veranstaltungen teilen
+			</p> -->
+		</div>
+		<img src="<?php echo get_template_directory_uri(); ?>/assets/images/willkommen-am-arrenberg_DE.png" alt="Willkommen auf deiner Quatiersplattform"/>	
+		<img src="<?php echo get_template_directory_uri(); ?>/assets/images/willkommen-am-arrenberg_IT.png" alt="Willkommen auf deiner Quatiersplattform"/>	
+		<img src="<?php echo get_template_directory_uri(); ?>/assets/images/willkommen-am-arrenberg_AR.png" alt="Willkommen auf deiner Quatiersplattform"/>	
+		<img src="<?php echo get_template_directory_uri(); ?>/assets/images/willkommen-am-arrenberg_TU.png" alt="Willkommen auf deiner Quatiersplattform"/>	
+		<img src="<?php echo get_template_directory_uri(); ?>/assets/images/willkommen-am-arrenberg_DE.png" alt="Willkommen auf deiner Quatiersplattform"/>	
+    </a>
+</div>
