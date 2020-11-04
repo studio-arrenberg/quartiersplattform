0d5adc18081e0ded2c837a5f1a72dcda6334216e
j0hannr <johann.rohn@gmail.com>
2020-11-04 11:38:12 +0100

    test

--- functions.php
+++ functions.php
@@ -70,22 +70,22 @@ function twentytwenty_theme_support() {
 	// set_post_thumbnail_size( 200, 150, true);
 	// with array( 'center', 'center' ) = (cropped to fit)
 
-	function add_custom_sizes() {
+	// function add_custom_sizes() {
 		add_theme_support( 'post-thumbnails' );
 		// square (1:1)
 		// add_image_size( 'square_s', 50, 50);
-		add_image_size( 'square_m', 180, 180, array( 'center', 'center' )); // this
-		add_image_size( 'square_l', 300, 300, array( 'center', 'center' )); // this
+		add_image_size( 'square_m', 180, 180, true); // this
+		add_image_size( 'square_l', 310, 310, array( 'center', 'center' )); // this
 		// preview (4:3)
 		// add_image_size( 'preview_s', 160, 120);
 		// add_image_size( 'preview_m', 200, 150, array( 'center', 'center' )); // this
-		add_image_size( 'preview_l', 800, 600, array( 'center', 'center' ));
+		// add_image_size( 'preview_l', 800, 600, array( 'center', 'center' ));
 		// landscape (2:1)
 		// add_image_size( 'landscape_s', 200, 100); 
 		// add_image_size( 'landscape_m', 400, 200);
 		// add_image_size( 'landscape_l', 970, 485);
-	}
-	add_action('after_setup_theme','add_custom_sizes');
+	// }
+	// add_action('after_setup_theme','add_custom_sizes');
 
 	function wpse_setup_theme() {
 		add_theme_support( 'post-thumbnails' );
