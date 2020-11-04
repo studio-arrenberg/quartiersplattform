08598b23ff98c6ed6120e43ccb68f2cf662c7963
j0hannr <johann.rohn@gmail.com>
2020-11-04 10:13:13 +0100

    hi

--- functions.php
+++ functions.php
@@ -78,7 +78,7 @@ function twentytwenty_theme_support() {
 		// preview (4:3)
 		// add_image_size( 'preview_s', 160, 120);
 		add_image_size( 'preview_m', 200, 150, array( 'center', 'center' )); // this
-		add_image_size( 'preview_l', 800, 600);
+		add_image_size( 'preview_l', 800, 600, array( 'center', 'center' ));
 		// landscape (2:1)
 		// add_image_size( 'landscape_s', 200, 100); 
 		// add_image_size( 'landscape_m', 400, 200);
