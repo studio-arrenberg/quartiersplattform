d9f65a9659a12e57494b491a49c518e13be94a86
camilomartins <66945701+camilomartins@users.noreply.github.com>
2020-11-10 17:46:01 +0100

    Update header.php

--- header.php
+++ header.php
@@ -154,7 +154,7 @@
         }
         else {
             ?>
-                <a  class="button header-button login button-has-icon is-style-outline" href="<?php echo get_site_url(); ?>/wp-admin">
+                <a  class="button header-button login button-has-icon is-style-outline" href="<?php echo get_site_url(); ?>/profil">
                     <img class="button-icon" src="<?php echo get_template_directory_uri()?>/assets/icons/profil.svg" />
                 </a>
             <?php 
