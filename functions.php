873d0cb890b0c23da3fb5f6e019f6639b301959a
j0hannr <johann.rohn@gmail.com>
2020-11-18 11:13:13 +0100

    fix

--- functions.php
+++ functions.php
@@ -1333,20 +1333,11 @@ function um_deregister_styles() {
 
   } add_action( 'wp_print_styles', 'um_deregister_styles', 100 );
 
-<<<<<<< HEAD
-// Activate WordPress Maintenance Mode
+  
+// // Activate WordPress Maintenance Mode
 function wp_maintenance_mode() {
 if (!current_user_can('edit_themes') || !is_user_logged_in()) {
 	wp_redirect('https://ap1.arrenberg.studio/maintenance.html');
 }
 }
 add_action('get_header', 'wp_maintenance_mode');
-=======
-// // Activate WordPress Maintenance Mode
-// function wp_maintenance_mode() {
-// if (!current_user_can('edit_themes') || !is_user_logged_in()) {
-// 	wp_redirect('https://ap1.arrenberg.studio/maintenance.html');
-// }
-// }
-// add_action('get_header', 'wp_maintenance_mode');
->>>>>>> c1921a879edd06c3f3659ef2337052c6c359aede
