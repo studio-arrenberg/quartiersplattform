a9c6331fd998d2b9231c39058b819edf74747217
j0hannr <johann.rohn@gmail.com>
2020-12-01 12:43:50 +0100

    http referer

--- header.php
+++ header.php
@@ -88,7 +88,7 @@
 
         <!-- back button -->
         <?php 
-        echo '<script>console.log("'.$_SERVER['HTTP_REFERER'].'");</script>';
+        echo '<script>console.log("'.$_SERVER['HTTP_REFERER']['host'].'");</script>';
         if (!$_SERVER['HTTP_REFERER']) {
             // display home button
             ?>
