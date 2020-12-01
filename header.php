b00cec52d4708f700fa49a663d98a98036bef614
j0hannr <johann.rohn@gmail.com>
2020-12-01 13:09:58 +0100

    http referer

--- header.php
+++ header.php
@@ -89,12 +89,14 @@
         <!-- back button -->
         <?php 
 
+        $my_url = parse_url($_SERVER['HTTP_REFERER'])['scheme'].parse_url($_SERVER['HTTP_REFERER'])['host'];
         if (parse_url($_SERVER['HTTP_REFERER'])['scheme'].parse_url($_SERVER['HTTP_REFERER'])['host'] == get_site_url()) {
             // echo "already here";
-            echo '<script>console.log("already here");</script>';
+            echo '<script>console.log("'.get_site_url().' my: '.$my_url.'");</script>';
         }
         else {
             echo '<script>console.log("newbie");</script>';
+            echo '<script>console.log("'.get_site_url().' my: '.$my_url.'");</script>';
         }
 
         echo '<script>console.log("'.$_SERVER['HTTP_REFERER'].'"); console.log("parsed: '.parse_url($_SERVER['HTTP_REFERER'])['host'].'");</script>';
