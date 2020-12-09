9433e3f9431fba8b5da7ea77894e1e78d159b146
j0hannr <johann.rohn@gmail.com>
2020-12-09 11:19:57 +0100

    quick addition of term project

--- functions.php
+++ functions.php
@@ -1392,3 +1392,52 @@ add_action( 'pre_get_posts', function ( $query ) {
         $query->set( 'meta_key', 'zeitpunkt' );
     }
 } );
+
+// create projekt in veranstaltung -> ort tax
+function update_veranstaltung_projekt($post_id) {
+
+    // only update terms if it's a post-type-B post
+    if ( 'projekte' != get_post_type($post_id)) {
+        return;
+    }
+    // don't create or update terms for system generated posts
+    if (get_post_status($post_id) == 'auto-draft') {
+        return;
+    }
+    /*
+    * Grab the post title and slug to use as the new 
+    * or updated term name and slug
+    */
+    $term_title = get_the_title($post_id);
+    $term_slug = get_post( $post_id )->post_name;
+    /*
+    * Check if a corresponding term already exists by comparing 
+    * the post ID to all existing term descriptions. 
+    */
+    $existing_terms = get_terms('veranstaltung_projekt', array(
+        'hide_empty' => false
+        )
+    );
+    foreach($existing_terms as $term) {
+        if ($term->description == $post_id) {
+            //term already exists, so update it and we're done
+            wp_update_term($term->term_id, 'veranstaltung_projekt', array(
+                'name' => $term_title,
+                'slug' => $term_slug
+                )
+            );
+            return;
+        }
+    }
+    /* 
+    * If we didn't find a match above, this is a new post, 
+    * so create a new term.
+    */
+    wp_insert_term($term_title, 'veranstaltung_projekt', array(
+        'slug' => $term_slug,
+        'description' => $post_id
+        )
+    );
+}
+//run the update function whenever a post is created or edited
+add_action('save_post', 'update_veranstaltung_projekt');
