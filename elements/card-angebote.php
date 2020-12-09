28404687a55d8b0a47564db98114916c51fd4b86
Moritz <moritz.windmann@me.com>
2020-12-09 16:12:19 +0100

    Fragen/ Angebote

--- elements/card-angebote.php
+++ elements/card-angebote.php
@@ -23,14 +23,18 @@
 // $comment_count = get_comment_count($post->ID)['approved'];
 ?>
 
-<div class="card shadow anmerkung ">
+<div class="card shadow ">
     <a href="<?php echo esc_url( get_permalink() ); ?>">
         <div class="content">
-        <div class="pre-title">Frage an das Quartier<span class="date"><?php echo get_the_date('j. F'); ?> <span></div>
-            <h3 class="card-title">
-                <?php shorten_title(get_the_title(), '60'); ?>
+            <div class="pre-title">Solidarisches Angebot <span class="date"><?php echo get_the_date('j. F'); ?> <span>
+            </div>
+            <h3 class="card-title-large">
+                <?php  shorten_title(get_field('text'), '200'); ?>
             </h3>
         </div>
+        <?php echo get_avatar( get_the_author_meta( 'ID' ), 15 ); ?>
+
+        <p><?php echo get_the_author_meta( 'user_firstname', get_the_author_meta( 'ID' ) ); ?></p>
+
     </a>
 </div>
-
