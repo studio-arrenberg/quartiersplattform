4fb73a9ccb95d9dba4015187225c7e0f700b8ca5
j0hannr <johann.rohn@gmail.com>
2020-12-10 19:11:30 +0100

    emoji picker jquery test

--- components/fragen.php
+++ components/fragen.php
@@ -49,7 +49,7 @@
     ?>
        
         <p class="lead emoji-picker-container">
-            <input type="email" class="form-control" placeholder="Input with max length of 10" data-emojiable="true"
+            <input id="emoji" type="email" class="form-control" placeholder="Input with max length of 10" data-emojiable="true"
                 maxlength="1">
         </p>
 
@@ -65,18 +65,32 @@
 
 
     <script>
-    $(function() {
-        // Initializes and creates emoji set from sprite sheet
-        window.emojiPicker = new EmojiPicker({
-            emojiable_selector: '[data-emojiable=true]',
-            assetsPath: '<?php echo get_template_directory_uri(); ?>/assets/emoji-picker/img/',
-            popupButtonClasses: 'fa fa-smile-o'
+
+        // picker for acf field
+        var el = $( "#acf-field_5fcf563d5b576" )
+        el.parent('div.acf-input-wrap').addClass('lead emoji-picker-container');
+        el.attr("data-emojiable", "true");
+        el.text('weweew');
+
+        // empty field
+        $( ".emoji-picker-container" ).click(function() {
+            $( this ).children('.emoji-wysiwyg-editor').text('');
         });
-        // Finds all elements with `emojiable_selector` and converts them to rich emoji input fields
-        // You may want to delay this step if you have dynamically created input fields that appear later in the loading process
-        // It can be called as many times as necessary; previously converted input fields will not be converted again
-        window.emojiPicker.discover();
-    });
+
+
+        $(function() {
+            // Initializes and creates emoji set from sprite sheet
+            window.emojiPicker = new EmojiPicker({
+                emojiable_selector: '[data-emojiable=true]',
+                assetsPath: '<?php echo get_template_directory_uri(); ?>/assets/emoji-picker/img/',
+                popupButtonClasses: 'fa fa-smile-o'
+            });
+            // Finds all elements with `emojiable_selector` and converts them to rich emoji input fields
+            // You may want to delay this step if you have dynamically created input fields that appear later in the loading process
+            // It can be called as many times as necessary; previously converted input fields will not be converted again
+            window.emojiPicker.discover();
+        });
+
     </script>
 
 
