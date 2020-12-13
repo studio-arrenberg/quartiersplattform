1a53f75c5e03af9b0acd3994ff24d679c7d70968
Moritz <moritz.windmann@me.com>
2020-12-13 12:01:32 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1531,3 +1531,7 @@ function comment_counter($id_post) {
 	}
 
 }
+
+//remove wp emojis
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' ); 
