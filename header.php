427ff9ca5c3a34d17825c4baa358380ad428e6ae
Moritz <moritz.windmann@me.com>
2020-12-16 17:28:11 +0100

    hide on scroll down

--- header.php
+++ header.php
@@ -218,3 +218,16 @@
 
 
     </header><!-- #site-header -->
+<script>
+
+    window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("site-header").style.top = "-45px";
+  } else {
+    document.getElementById("site-header").style.top = "0px";
+  }
+}
+
+</script>
