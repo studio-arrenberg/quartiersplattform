2f3ef080d86c40eabcb179d330bd4f154969018c
camilomartins <66945701+camilomartins@users.noreply.github.com>
2021-01-17 13:51:20 +0100

    Bug fixing project form

--- pages/single-projekte.php
+++ pages/single-projekte.php
@@ -201,14 +201,14 @@ else {
                 'form' => true,
                 'return' => '%post_url%',
                 'submit_value' => 'Änderungen speichern',
-                'post_title' => false,
+                'post_title' => true,
                 'post_content' => false,    
                 'fields' => array(
                     // fehlt: titel, beschreibung
                     'target',
                     'emoji',
-                    'text',
                     'slogan',
+                    'description',
                     '_thumbnail_id', // Naming Bild ≠ Bilder
                 )
             )
