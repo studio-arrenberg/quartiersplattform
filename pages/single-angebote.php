a56e6c7b58d2a9afd4fab98b19039b1c39aacde9
Moritz <moritz.windmann@me.com>
2021-01-20 22:32:04 +0100

    Update single-angebote.php

--- pages/single-angebote.php
+++ pages/single-angebote.php
@@ -36,7 +36,7 @@ if ( have_posts() ) {
 
     </div>
     <br>
-    <h2>Johann kontaktieren </h2>
+    <h4>Kontakt</h4>
     <!-- kontakt -->
     <?php if (is_user_logged_in()) { ?>
     <a class="button is-style-outline" href="tel:<?php the_field('phone'); ?>"><?php the_field('phone'); ?></a>
