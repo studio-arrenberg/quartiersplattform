e4a310318da3f87c18b3e5cd4a7d99c2cad24d24
Moritz <moritz.windmann@me.com>
2021-01-20 22:46:18 +0100

    Update call-gemeinsam.php

--- components/call-gemeinsam.php
+++ components/call-gemeinsam.php
@@ -13,8 +13,8 @@
 <div class="card  bg_blue landscape shadow">
     <a href="<?php echo get_site_url(); ?>/gemeinsam/">
         <div class="content white-text">
-            <!-- <div class="emojis-top">🛠 🎁 🚀</div> -->
-            <div class="emojis-top"><img draggable="false" role="img" class="emoji" alt="🙋🏼‍♂️" src="https://s.w.org/images/core/emoji/13.0.1/svg/1f64b-1f3fc-200d-2642-fe0f.svg"><img draggable="false" role="img" class="emoji" alt="📑" src="https://s.w.org/images/core/emoji/13.0.1/svg/1f4d1.svg"><img draggable="false" role="img" class="emoji" alt="💭" src="https://s.w.org/images/core/emoji/13.0.1/svg/1f4ad.svg"></div>
+            <div class="emojis-top">🛠 🎁 🚀</div>
+            <!-- <div class="emojis-top"><img draggable="false" role="img" class="emoji" alt="🙋🏼‍♂️" src="https://s.w.org/images/core/emoji/13.0.1/svg/1f64b-1f3fc-200d-2642-fe0f.svg"><img draggable="false" role="img" class="emoji" alt="📑" src="https://s.w.org/images/core/emoji/13.0.1/svg/1f4d1.svg"><img draggable="false" role="img" class="emoji" alt="💭" src="https://s.w.org/images/core/emoji/13.0.1/svg/1f4ad.svg"></div> -->
             <h3 class="card-title">
                 Dein Quartier wird zur <br> Sharing-Community
             </h3>
