50ae2128568904620ee589b59b7c347907d807b5
j0hannr <johann.rohn@gmail.com>
2021-02-09 12:01:37 +0100

    user cookie fix

--- functions.php
+++ functions.php
@@ -1906,17 +1906,8 @@ function wpdev_login_session( $expire ) { // Set login session limit in seconds
 add_action('init', 'set_user_cookie_inc_guest');
 function set_user_cookie_inc_guest(){
 
-	# check if user is logged in
-	if(is_user_logged_in()) {
-		# set user cookie
-        // wp_set_auth_cookie( get_current_user_id(), true, is_ssl() ); 
-		# different approche
-		if (wp_validate_auth_cookie()==FALSE) {
-			wp_set_auth_cookie( get_current_user_id() , true, is_ssl());
-		}
-	}
 	# check if cookie not set
-    else if (!isset($_COOKIE['guest'])) {
+    	if (!isset($_COOKIE['guest'])) {
 		# get/increase or set guest counter
 		if (!get_option('guest_counter')) {
 			add_option('guest_counter', 1);
