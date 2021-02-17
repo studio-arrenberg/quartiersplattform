ea70e0b01ab39d05cd8814131d1b0f8f2d4de85b
j0hannr <johann.rohn@gmail.com>
2021-02-17 16:33:51 +0100

    smart card projekt

--- components/smart-card/projekte.php
+++ components/smart-card/projekte.php
@@ -1,3 +1,54 @@
+
+<?php
+    $args4 = array(
+        'post_type'=> array('projekte'), 
+        'post_status'=>'publish', 
+        'author' =>  $current_user->ID,
+        'posts_per_page'=> -1, 
+        'order' => 'DESC',
+        'offset' => '0', 
+    );
+    $my_query = new WP_Query($args4);
+    if ($my_query->post_count > 0) {
+        
+
+        $args4 = new WP_Query($args4);
+        while ( $args4->have_posts() ) {
+
+
+            ?>
+
+                <div class="card smart-card shadow">
+                    <div class="card-header">
+                        <h2>Dein Projekt</h2>
+                    </div>
+            
+            <?php
+
+            $args4->the_post();
+            get_template_part('elements/card', get_post_type());
+
+            ?>
+                    <div class="card-footer">
+                        <a class="button card-button" href="<?php echo get_site_url(); ?>/nachricht-erstellen/?project=<?php echo $post->post_name; ?>">Update veröffentlichen</a>
+                        <a class="button card-button" href="<?php echo get_site_url(); ?>/veranstaltung-erstellen/?project=<?php echo $post->post_name; ?>">Veranstaltung erstellen</a>
+                    </div>
+
+                </div>
+            
+            <?php
+        }
+        wp_reset_postdata();
+
+
+        
+    }
+?>
+
+
+
+
+
 <div class="card smart-card shadow">
     <div class="card-header">
         <h2>Dein Projekt</h2>
