aea36778700af4aa5f8bbbdd9699ab80bfa6e6ba
j0hannr <johann.rohn@gmail.com>
2021-03-01 14:59:15 +0100

    removed function due to error

--- setup/immigration.php
+++ setup/immigration.php
@@ -4,59 +4,68 @@
  * 
  *  Imigration Setup
  * 
- *  1. Poll -> Umfragen Post Type Naming
+ * 	1. Veranstaltungen rebuild datetime field
+ *  2. Poll -> Umfragen Post Type Naming
  * 
  */
 
 
 /**
  *  --------------------------------------------------------
- *  1. Poll -> Umfragen Post Type Naming
+ *  2. Poll -> Umfragen Post Type Naming
  *  --------------------------------------------------------
  */
 
 
 function update_poll_naming() {
 
-    global $wpdb;    
+    global $wpdb;   
+	# [ ] execute if posts table exists  
     $result = $wpdb->get_results( "UPDATE `wp_posts` SET  `post_type` =  'umfragen' WHERE `post_type` = 'poll'");
 
 }
-add_action( 'init', 'update_poll_naming' );
+// add_action( 'init', 'update_poll_naming' );
 
 
-// Veranstaltungen rebuild datetime field
-function veranstaltungen_field_zeitpunkt() {
+/**
+ *  --------------------------------------------------------
+ *  1. Veranstaltungen rebuild datetime field
+ *  --------------------------------------------------------
+ */
+
+
+# Veranstaltungen rebuild datetime field
+// function veranstaltungen_field_zeitpunkt() {
 		
-	$args3 = array(
-		'post_type'=>'veranstaltungen', 
-		'post_status'=>'publish', 
-		'posts_per_page'=> -1,
-		'meta_query' => array(
-			'relation' => 'OR',
-			array(
-				'key' => 'event_date',
-				'value' => 'No',
-				'compare' => 'LIKE'
-			),
-			array(
-				'key' => 'event_date',
-				'compare' => 'NOT EXISTS'
-			)
-		)
-	);
+// 	$args3 = array(
+// 		'post_type'=>'veranstaltungen', 
+// 		'post_status'=>'publish', 
+// 		'posts_per_page'=> -1,
+// 		'meta_query' => array(
+// 			'relation' => 'OR',
+// 			array(
+// 				'key' => 'event_date',
+// 				'value' => 'No',
+// 				'compare' => 'LIKE'
+// 			),
+// 			array(
+// 				'key' => 'event_date',
+// 				'compare' => 'NOT EXISTS'
+// 			)
+// 		)
+// 	);
 
-	$query2 = new WP_Query($args3);
+// 	$query2 = new WP_Query($args3);
 
-	while ( $query2->have_posts() ) {
-		$query2->the_post();
+// 	while ( $query2->have_posts() ) {
+// 		$query2->the_post();
 
-		update_post_meta(get_the_id(), 'event_date', date("Y-m-d",strtotime(get_field('zeitpunkt'))));
-		update_post_meta(get_the_id(), 'event_time', date("H:i:s",strtotime(get_field('zeitpunkt'))));
-		update_post_meta(get_the_id(), 'event_end_time', date("H:i:s",strtotime(get_field('ende'))));
+// 		update_post_meta(get_the_id(), 'event_date', date("Y-m-d",strtotime(get_field('zeitpunkt'))));
+// 		update_post_meta(get_the_id(), 'event_time', date("H:i:s",strtotime(get_field('zeitpunkt'))));
+// 		update_post_meta(get_the_id(), 'event_end_time', date("H:i:s",strtotime(get_field('ende'))));
 
-	}
-	wp_reset_postdata();
+// 	}
+// 	wp_reset_postdata();
 
-}
-add_action( 'init', 'veranstaltungen_field_zeitpunkt' );
+// }
+// add_action( 'init', 'veranstaltungen_field_zeitpunkt' );
