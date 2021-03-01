148fe674ec06116777b5aa166cbbc9c6200dbf67
j0hannr <johann.rohn@gmail.com>
2021-03-01 15:04:24 +0100

    removed function due to error

--- setup/immigration.php
+++ setup/immigration.php
@@ -20,11 +20,12 @@
 function update_poll_naming() {
 
     global $wpdb;   
+	$table = $table_prefix."posts";
 	# [ ] execute if posts table exists  
-    $result = $wpdb->get_results( "UPDATE `wp_posts` SET  `post_type` =  'umfragen' WHERE `post_type` = 'poll'");
+    $result = $wpdb->get_results( "UPDATE `$table` SET  `post_type` =  'umfragen' WHERE `post_type` = 'poll'");
 
 }
-// add_action( 'init', 'update_poll_naming' );
+add_action( 'init', 'update_poll_naming' );
 
 
 /**
