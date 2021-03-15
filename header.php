f288d80cdafa566e4c02de21b3d923f1dad8963a
Moritz <moritz.windmann@me.com>
2021-03-15 20:14:53 +0100

    Update header.php

--- header.php
+++ header.php
@@ -10,7 +10,6 @@ wp_maintenance_mode();
 <html class="no-js" <?php language_attributes(); ?>>
 
 <head>
-
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
@@ -19,32 +18,19 @@ wp_maintenance_mode();
 
     <?php wp_head(); ?>
 
-    <link rel="apple-touch-icon" sizes="57x57"
-        href="<?php echo get_template_directory_uri()?>/assets/favicon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60"
-        href="<?php echo get_template_directory_uri()?>/assets/favicon/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72"
-        href="<?php echo get_template_directory_uri()?>/assets/favicon/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76"
-        href="<?php echo get_template_directory_uri()?>/assets/favicon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114"
-        href="<?php echo get_template_directory_uri()?>/assets/favicon/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120"
-        href="<?php echo get_template_directory_uri()?>/assets/favicon/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144"
-        href="<?php echo get_template_directory_uri()?>/assets/favicon/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152"
-        href="<?php echo get_template_directory_uri()?>/assets/favicon/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180"
-        href="<?php echo get_template_directory_uri()?>/assets/favicon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"
-        href="<?php echo get_template_directory_uri()?>/assets/favicon/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32"
-        href="<?php echo get_template_directory_uri()?>/assets/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96"
-        href="<?php echo get_template_directory_uri()?>/assets/favicon/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16"
-        href="<?php echo get_template_directory_uri()?>/assets/favicon/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri()?>/assets/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri()?>/assets/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri()?>/assets/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri()?>/assets/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri()?>/assets/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri()?>/assets/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri()?>/assets/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri()?>/assets/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri()?>/assets/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="<?php echo get_template_directory_uri()?>/assets/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri()?>/assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_template_directory_uri()?>/assets/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri()?>/assets/favicon/favicon-16x16.png">
     <link rel="manifest" href="<?php echo get_template_directory_uri()?>/assets/favicon/manifest.json">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage"
@@ -56,7 +42,6 @@ wp_maintenance_mode();
 
     <!-- Matomo Tracking API Key -->
     <?php the_field('matomo_api', 'option'); ?>
-
 </head>
 
 <body <?php body_class(); ?>>
