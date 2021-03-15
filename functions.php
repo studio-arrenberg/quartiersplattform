f33fe13909dfb6b302b2ced2cf509a94b02f0ac3
Moritz <41573605+moritzwindmann@users.noreply.github.com>
2021-03-15 11:23:47 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -31,6 +31,9 @@
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
+
+
+
 function twentytwenty_theme_support() {
 
 	// Add default posts and comments RSS feed links to head.
@@ -1869,3 +1872,4 @@ function set_user_cookie_inc_guest(){
     }  
 
 }
+
