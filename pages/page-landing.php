ced114579dacb1f118b7747a796daea337cdbf64
Moritz <41573605+moritzwindmann@users.noreply.github.com>
2021-03-16 16:03:31 +0100

    CSS Grid, Sitebar, New Header

--- pages/page-landing.php
+++ pages/page-landing.php
@@ -322,4 +322,24 @@ else {
 
 </main><!-- #site-content -->
 
+<div class="right-sidebar">
+		<?php 
+			// Smart cards 
+			// Angebote und fragen
+			get_template_part('components/smart-card/angebote-fragen');
+			// Projekte
+			get_template_part('components/smart-card/projekte');
+		?>
+			
+			<div class="card ">
+			<div class="emojis-top">🍾🍰
+            <div class="card-header">
+            <h2>Wir laufen jetzt unter <span class="highlight">CSS GRID</span></h2>
+			<p>Es gibt eine Sitebar die beliebig erweitert werden kann</p>
+            </div>
+
+           
+        </div>
+	</div>
+
 <?php get_footer(); ?>
