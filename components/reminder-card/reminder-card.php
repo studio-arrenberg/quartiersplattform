692b072d168f607e687e9b435cb18edf855f5cf0
j0hannr <johann.rohn@gmail.com>
2021-03-25 11:03:39 +0100

    checkout before reminder card

---
+++ components/reminder-card/reminder-card.php
@@ -0,0 +1,14 @@
+
+<div class="card card-large">
+	<div class="content content-shrink">
+		<h1 class="card-title-large">
+			Neuigkeiten und Projektupdates
+		</h1>
+		<h3>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt sed veritatis et quibusdam molestiae repellendus fugiat in dolorum. Tempore illo eum itaque voluptate, nulla exercitationem laborum placeat eius odio possimus?	
+		</h3>
+	</div>
+	<a class="close-card-link" href="#">
+		<img class="close-card-icon" src="<?php echo get_template_directory_uri()?>/assets/icons/close.svg" />
+	</a>
+</div>
