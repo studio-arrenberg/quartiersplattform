7f39eb97b8c38101964bc118b387ddd485730733
j0hannr <johann.rohn@gmail.com>
2021-04-27 15:45:11 +0200

    sdgs colors

--- pages/page-sdg.php
+++ pages/page-sdg.php
@@ -27,13 +27,14 @@ get_header();
 	while ( $args->have_posts() ) {
 		$args->the_post();
 
-		$color = get_post_meta( get_the_ID(), 'color', true );
-
 		?>
-			<div id="sdg-card" style="color: <?php echo $color; ?>;" class="card card-sgd shadow <?php echo get_post_meta( get_the_ID(), 'class', true ); ?>">
+			<div id="sdg-card" style="background-color: <?php the_field('color'); ?>;" class="card card-sgd shadow">
                 <a class="card-link" >
 
                     <div class="content">    
+						<h1>
+							<?php the_field('goal'); ?>
+						</h1>
 						<h3 class="card-title">
                         	<?php the_title(); ?>
                         </h3>
