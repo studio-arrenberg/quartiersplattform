3ace60cd056f73dcdc46a74a098d260c599846a4
j0hannr <52762073+j0hannr@users.noreply.github.com>
2021-04-28 11:29:46 +0200

    Minified 25 files

--- assets/js/editor-script-block.js
+++ assets/js/editor-script-block.js
@@ -1,9 +1,1 @@
-/**
- * Remove squared button style
- *
- * @since Twenty Twenty 1.0
- */
-/* global wp */
-wp.domReady( function() {
-	wp.blocks.unregisterBlockStyle( 'core/button', 'squared' );
-} );
+wp.domReady((function(){wp.blocks.unregisterBlockStyle("core/button","squared")}));
