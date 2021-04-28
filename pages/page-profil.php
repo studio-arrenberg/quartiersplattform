437f463afead3ed4f20b06409aab5254dee61ca5
j0hannr <johann.rohn@gmail.com>
2021-04-28 21:01:42 +0200

    profile clean up

--- pages/page-profil.php
+++ pages/page-profil.php
@@ -109,99 +109,57 @@ $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : g
 
         <div id="settings" class="bar bar-hidden">
 
-        <?php if( current_user_can('administrator') ) { ?>
-            <h2>Reminder Cards</h2>
-            <p>Zeige alle Reminder Cards wieder an.</p>
-            <div class="reset_reminder_cards">
-                <a class="button reset_reminder_cards" onclick="reset_reminder_cards()">Reminder Cards zurücksetzen</a>
-                <span class="acf-spinner" style="display: inline-block;"></span>
-            </div>
-            
-            <script>
-                function reset_reminder_cards() {
 
-                    $('div.reset_reminder_cards span.acf-spinner').addClass('is-active');
+                <?php get_template_part( 'components/profil/reminder_card_reset'); ?>
 
-                    var ajax_url = "<?= admin_url('admin-ajax.php'); ?>";
+                <h4>Einstellungen</h4>
+                <!-- Contact Information -->   
+                <h2>Bearbeite deine Kontaktinformationen</h2>
+                <br>
+                    <?php
+                    $userid = "user_".$current_user->ID; 
+                    acf_form (
+                        array(
+                            'form' => true,
+                            'post_id' => $userid,
+                            'return' => get_site_url()."/profil"."/",
+                            'submit_value' => 'Änderungen speichern',
+                            'post_title' => false,
+                            'post_content' => false,    
+                            'field_groups' => array('group_6034e1d00f273'),
+                        )
+                    );
+                    ?>
+                <br>
+                <br>
+                <!-- Biography Information -->   
+                <h2>Erzähle etwas über dich</h2>
+                <br>
+                    <?php
+                    $userid = "user_".$current_user->ID; 
+                    acf_form (
+                        array(
+                            'form' => true,
+                            'post_id' => $userid,
+                            'return' => get_site_url()."/profil"."/",
+                            'submit_value' => 'Änderungen speichern',
+                            'post_title' => false,
+                            'post_content' => false,    
+                            'field_groups' => array('group_605dc2bb690d9'),
+                        )
+                    );
+                    ?>
 
-                    var data = {
-                        'action': 'reset_reminder_cards',
-                        'request': 1,
-                        _ajax_nonce: '<?php echo wp_create_nonce( 'my_ajax_nonce' ); ?>'
-                    };
+                <h2>Profil bearbeiten</h2>
+                <?php echo do_shortcode("[ultimatemember_account]"); ?>
 
-                    $.ajax({
-                        url: ajax_url,
-                        type: 'post',
-                        data: data,
-                        dataType: 'json',
-                        success: function(response){
-                            console.log(response);
-                            $('a.reset_reminder_cards').addClass('is-done');
-                            $('div.reset_reminder_cards span.acf-spinner').removeClass('is-active');
-                        }
-                    });
-                }
-            </script>
-        <?php } ?>
+                <a class="button is-style-outline" href="<?php echo get_author_posts_url(get_current_user_id()); ?>">Mein öffentliches Profil ansehen</a>
 
-
-            <h4>Einstellungen</h4>
-            <!-- Contact Information -->   
-            <h2>Bearbeite deine Kontaktinformationen</h2>
-            <br>
-                <?php
-                $userid = "user_".$current_user->ID; 
-                acf_form (
-                    array(
-                        'form' => true,
-                        'post_id' => $userid,
-                        'return' => get_site_url()."/profil"."/",
-                        'submit_value' => 'Änderungen speichern',
-                        'post_title' => false,
-                        'post_content' => false,    
-                        'field_groups' => array('group_6034e1d00f273'),
-                    )
-                );
-                ?>
-            <br>
-            <br>
-            <!-- Biography Information -->   
-            <h2>Erzähle etwas über dich</h2>
-            <br>
-                <?php
-                $userid = "user_".$current_user->ID; 
-                acf_form (
-                    array(
-                        'form' => true,
-                        'post_id' => $userid,
-                        'return' => get_site_url()."/profil"."/",
-                        'submit_value' => 'Änderungen speichern',
-                        'post_title' => false,
-                        'post_content' => false,    
-                        'field_groups' => array('group_605dc2bb690d9'),
-                    )
-                );
-                ?>
-        </div>
-
-        <div id="edit" class="bar bar-hidden">
-            <h4>Bearbeiten</h4>
-
-
-            <h2>Profil bearbeiten</h2>
-            <?php echo do_shortcode("[ultimatemember_account]"); ?>
-
-            <a class="button is-style-outline" href="<?php echo get_author_posts_url(get_current_user_id()); ?>">Mein öffentliches Profil ansehen</a>
-
-            <br>
-            
-            <?php if (is_user_logged_in()) : ?>
+                <br>
+                
                 <a class="button" href="<?php echo get_site_url().'/logout/'; ?>">Logout</a>
-            <?php endif;?>
-
-
         </div>
+
     </div>
 
     
