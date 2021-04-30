6b5ebabbac47709950021cbca33f6931772a2d97
Moritz <moritz.windmann@me.com>
2021-04-30 13:51:42 +0200

    Yours

--- elements/badge-projekte.php
+++ elements/badge-projekte.php
@@ -9,8 +9,8 @@
 ?>
 
 <!-- <div class="card-group"> -->
-<a class="badge-link <?php if (get_query_var( 'highlight_display') === true && get_queried_object_id() ==  get_the_ID(  )) echo "highlight"; ?>" href="<?php echo esc_url( get_permalink() ); ?>">
-    <div class="badge shadow">
+<a class="badge-link" href="<?php echo esc_url( get_permalink() ); ?>">
+    <div class="badge shadow  <?php if (get_query_var( 'highlight_display') === true && get_queried_object_id() ==  get_the_ID(  )) echo "yours"; ?>">
         <span class="emoji"><?php  shorten(get_field('emoji'), '200'); ?></span> 
     </div>
     <h3 class="card-title-small"><?php shorten(get_the_title(), '60'); ?></h3>
