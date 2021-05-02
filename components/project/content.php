a0fb3d510fef8f9567d3501e70b6d991743eb1fe
camilomartins <66945701+camilomartins@users.noreply.github.com>
2021-05-02 16:11:52 +0200

    Update content.php

--- components/project/content.php
+++ components/project/content.php
@@ -1,10 +1,10 @@
 <?php global $current_user; ?>
 
 <?php
-if ( ( is_user_logged_in() && $current_user->ID == $post->post_author ) ) {
+if( ( is_user_logged_in() && $current_user->ID == $post->post_author ) ) {
 ?>
-
-
+<a class="button is-style-outline" href="<?php get_permalink(); ?>?action=edit"><?php _e('Projekt bearbeiten', 'quartiersplattform'); ?> </a>
+<?php } ?>
 
 <?php if (get_field('text')) { ?>
 <div class="single-content">
