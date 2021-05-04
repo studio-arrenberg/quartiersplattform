085ab7f29c975d3a295b027dc0c5e7de799109fb
Moritz <moritz.windmann@me.com>
2021-05-04 15:23:47 +0200

    Author

--- author.php
+++ author.php
@@ -11,93 +11,114 @@ get_header();
 
 ?>
 
-<main id="site-content" class="center-header-template <?php if (!has_post_thumbnail()) echo "no-single-header-image"; ?>" role="main">
 
-    <div class="center-header">
 
-        <img class="center-header-image" src="<?php echo get_avatar_url( $curauth->ID ) ?>" />
 
-        <div class="center-header-content center-mobile">
+<main id="site-content" class="page-grid  <?php if (!has_post_thumbnail()) echo "no-single-header-image"; ?>" role="main">
 
-            <h1><?php echo $curauth->first_name." ".$curauth->last_name; ?></h1>
-            <br>
+	<div class="left-sidebar">
 
-            <?php 
-                if ($curauth->ID == get_current_user_id()) {
-                    ?>
-                        <a class="button" href="<?php echo get_site_url(); ?>/profil/"><?php _e('Mein Profil bearbeiten', 'quartiersplattform'); ?></a>
-                    <?php 
+		<div class="hidden-small">
+
+			<?php 
+				$args4 = array(
+					'post_type'=> array('projekte'), 
+					'post_status'=>'publish', 
+					'posts_per_page'=> 20,
+					'orderby' => 'date'
+				);
+			?>  
+
+			<?php // card_list($args4); ?>
+
+            <?php projekt_carousel(); ?>
+
+		</div>
+
+		
+	</div>
+
+
+	<div class="main-content">
+        <div class="center-header">
+            <img class="center-header-image" src="<?php echo get_avatar_url( $curauth->ID ) ?>" />
+
+            <div class="center-header-content center-mobile">
+
+                <h1><?php echo $curauth->first_name." ".$curauth->last_name; ?></h1>
+                <br>
+
+                <?php 
+                    if ($curauth->ID == get_current_user_id()) {
+                        ?>
+                            <a class="button" href="<?php echo get_site_url(); ?>/profil/"><?php _e('Mein Profil bearbeiten', 'quartiersplattform'); ?></a>
+                        <?php 
+                    }
+                ?>
+
+                <!-- Kontakt  -->
+                <?php 
+                
+                $userid = "user_".$curauth->ID; 
+                // echo $curauth->ID;
+                author_card(true, $curauth->ID);
+                
+                ?>
+
+                
+                <!-- About -->
+                <?php if( get_field('about', $userid) ) { ?>
+                    <br>
+                    <br>
+                    <h2><?php _e('Über mich', 'quartiersplattform'); ?> </h2>
+                    <div><?php the_field('about', $userid); ?></div>
+                <?php } ?>
+
+            </div>
+            </div>
+    
+
+            <?php
+                // Projekte
+                $args4 = array(
+                    'post_type'=> array('projekte'), 
+                    'post_status'=>'publish', 
+                    'author' =>  $curauth->ID,
+                    'posts_per_page'=> -1, 
+                    'order' => 'DESC',
+                    'offset' => '0', 
+                );
+                $my_query = new WP_Query($args4);
+                if ($my_query->post_count > 0) {
+                    
+                    echo "<h2>".__("Projekte",'quartiersplattform')."</h2>";
+                    // slider($args4, $type = 'card', $slides = '1', $dragfree = 'false', $align = 'start');          
+                    card_list($args4);      
+                    
                 }
             ?>
 
-            <!-- Kontakt  -->
-            <?php 
-            
-            $userid = "user_".$curauth->ID; 
-            // echo $curauth->ID;
-            author_card(true, $curauth->ID);
-            
+            <?php
+                $args4 = array(
+                    'post_type'=> array('veranstaltungen', 'nachrichten', 'projekte', 'umfragen'), 
+                    'post_status'=>'publish', 
+                    'author' =>  $curauth->ID,
+                    'posts_per_page'=> 20, 
+                    'order' => 'DESC',
+                    'offset' => '0', 
+                );
+                $my_query = new WP_Query($args4);
+                if ($my_query->post_count > 0) {
+    
+                    card_list($args4);      
+                    // get_template_part('elements/'.$type.'', get_post_type());
+                    
+                }
             ?>
 
-            
-            <!-- About -->
-            <?php if( get_field('about', $userid) ) { ?>
-                <br>
-                <br>
-                <h2><?php _e('Über mich', 'quartiersplattform'); ?> </h2>
-                <div><?php the_field('about', $userid); ?></div>
-            <?php } ?>
-
-
+        </div>
         </div>
     </div>
-    </div>
-
-
-    <div class="single-content-fullwith">
-
-        <?php
-            // Projekte
-            $args4 = array(
-                'post_type'=> array('projekte'), 
-                'post_status'=>'publish', 
-                'author' =>  $curauth->ID,
-                'posts_per_page'=> -1, 
-                'order' => 'DESC',
-                'offset' => '0', 
-            );
-            $my_query = new WP_Query($args4);
-            if ($my_query->post_count > 0) {
-                
-                echo "<h2>".__("Projekte",'quartiersplattform')."</h2>";
-                // slider($args4, $type = 'card', $slides = '1', $dragfree = 'false', $align = 'start');          
-                card_list($args4);      
-                
-            }
-        ?>
-
-        <?php
-            // Angebote und Fragen
-            // $args4 = array(
-            //     'post_type'=> array('angebote', 'fragen'), 
-            //     'post_status'=>'publish', 
-            //     'author' =>  $curauth->ID,
-            //     'posts_per_page'=> 20, 
-            //     'order' => 'DESC',
-            //     'offset' => '0', 
-            // );
-            // $my_query = new WP_Query($args4);
-            // if ($my_query->post_count > 0) {
-  
-            //     echo "<h2>Fragen und Angebote</h2>";
-            //     slider($args4, $type = 'card', $slides = '1', $dragfree = 'false');
-            //     // get_template_part('elements/'.$type.'', get_post_type());
-                
-            // }
-        ?>
-
-    </div>
-
-
+            </main>
 </div>
 <?php get_footer(); ?>
