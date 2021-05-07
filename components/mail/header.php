474d53d810699d6752403f1a4cbeae4f83da84bc
j0hannr <johann.rohn@gmail.com>
2021-05-07 13:36:04 +0200

    mail for feedback

--- components/mail/header.php
+++ components/mail/header.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
 <html>
-  <head>
+
+<head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    
-  </head>
-  <body>
-    <h1>Hello World</h1>
-  </body>
-</html>
+</head>
+
+<body>
+    <div style="word-wrap: break-word; -webkit-nbsp-mode: space; line-break: after-white-space;">
+
+        <table width="600px" style="color: rgb(0, 0, 0); font-size: 12px" class="">
+            <tbody class="">
