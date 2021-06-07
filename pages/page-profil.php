71841a853cd0e0621e8edd16beb11bb0d538803f
camilomartins <66945701+camilomartins@users.noreply.github.com>
2021-06-07 12:14:23 +0200

    Translation Bugfixing

--- pages/page-profil.php
+++ pages/page-profil.php
@@ -24,6 +24,45 @@ if (!is_user_logged_in()) {
 <main id="site-content" class="page-grid " role="main">
 
 	<div class="left-sidebar">
+    
+
+    <div class="single-header profil">
+        
+        <?php 
+            // User Avatar
+            $current_user = wp_get_current_user();
+            echo get_avatar( $current_user->user_email, 32 );
+        ?>
+
+        <!-- user name -->
+        <div class="single-header-content">
+            <h1><?php $current_user = wp_get_current_user(); echo $current_user->first_name." ".$current_user->last_name; ?></h1>
+        </div>
+
+        
+    </div>
+    <?php 
+        
+        $options = array(
+            'post_id' => 'user_'.$current_user->ID,
+            'field_groups' => array(77),
+            'form' => true, 
+            'return' => add_query_arg( 'updated', 'true', get_permalink() ), 
+            'html_before_fields' => '',
+            'html_after_fields' => '',
+            'submit_value' => 'Test' 
+        );
+        acf_form( $options );
+        echo $current_user->ID;
+
+        if (user_can( $current_user, 'administrator')) {
+            echo do_shortcode('[avatar_upload user="'.$user->ID.'"]');
+        }
+        else {
+            echo do_shortcode('[avatar_upload]');
+        }
+        
+    ?>
 
 		<div class="hidden-small">
 			<?php 
@@ -208,6 +247,58 @@ if (!is_user_logged_in()) {
             <br>
             <a class="button" href="<?php echo get_site_url().'/logout/'; ?>"><?php _e('Abmelden', 'quartiersplattform'); ?> </a>
             <br><br>
+                <?php get_template_part( 'components/profil/reminder_card_reset'); ?>
+
+                <h4>Einstellungen</h4>
+                <!-- Contact Information -->   
+                <h2>Bearbeite deine Kontaktinformationen</h2>
+                <br>
+                    <?php
+                    $userid = "user_".$current_user->ID; 
+                    acf_form (
+                        array(
+                            'form' => true,
+                            'post_id' => $userid,
+                            'return' => get_site_url()."/profil"."/",
+                            'submit_value' => 'Änderungen speichern',
+                            'post_title' => false,
+                            'post_content' => false,    
+                            'field_groups' => array('group_6034e1d00f273'),
+                        )
+                    );
+                    ?>
+                <br>
+                <br>
+                <!-- Biography Information -->   
+                <h2>Erzähle etwas über dich</h2>
+                <br>
+                    <?php
+                    $userid = "user_".$current_user->ID; 
+                    acf_form (
+                        array(
+                            'form' => true,
+                            'post_id' => $userid,
+                            'return' => get_site_url()."/profil"."/",
+                            'submit_value' => 'Änderungen speichern',
+                            'post_title' => false,
+                            'post_content' => false,    
+                            'field_groups' => array('group_605dc2bb690d9'),
+                        )
+                    );
+                    ?>
+                <?php echo do_shortcode('[avatar_upload id='.$user_id.']'); ?>
+                
+                <h2>Profil bearbeiten</h2>
+                <?php echo do_shortcode("[ultimatemember_account]"); ?>
+                
+
+
+
+                <a class="button is-style-outline" href="<?php echo get_author_posts_url(get_current_user_id()); ?>">Mein öffentliches Profil ansehen</a>
+
+                <br>
+                
+                <a class="button" href="<?php echo get_site_url().'/logout/'; ?>">Logout</a>
         </div>
 
     </div>
