6006c5790ef2354a9a504eadfb86df15c1bca191
camilomartins <66945701+camilomartins@users.noreply.github.com>
2021-06-14 11:25:48 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -3299,11 +3299,12 @@ function qp_detect_language() {
 			return qp_detect_browser_language();
 		}	
 	}else{
-		// // check user locale setting
-		// if(!empty($_GET['lang'])){
-		// 	setcookie('language',  $_GET['lang'], time()+62208000, COOKIEPATH, COOKIE_DOMAIN);
-		// 	update_user_meta(get_current_user_id(), 'locale', $_GET['lang']);
-		// 	return $_GET['lang'];
+		// check user locale setting
+		if(!empty($_GET['lang'])){
+			setcookie('language',  $_GET['lang'], time()+62208000, COOKIEPATH, COOKIE_DOMAIN);
+			update_user_meta(get_current_user_id(), 'locale', $_GET['lang']);
+			return $_GET['lang'];
+		}
 		// }else{
 		// 	// Notice: Undefined variable: current_user
 		// 	// Notice: Trying to get property 'ID' of non-object
