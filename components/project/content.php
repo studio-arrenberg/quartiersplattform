fa47ce57ecc538879991e5038fe6f7eb3271b8a4
Moritz <41573605+moritzwindmann@users.noreply.github.com>
2022-12-15 13:30:41 +0100

    Update content.php

--- components/project/content.php
+++ components/project/content.php
@@ -6,28 +6,22 @@
         <p><?php extract_links(get_field('text')); ?></p>
     </div>
 <?php } ?>
-
 <?php 
 
-if (!get_field('text') && qp_project_owner()) {
-    $text = __('Du kannst deinem Projekt eine Beschreibung hinzufügen, um die Ziele und Inhalte noch besser zu erklären.','quartiersplattform');
-    reminder_card('projekt-description-reminder'.get_the_ID(), __('Beschreibung hinzufügen','quartiersplattform'), $text);
-}
+    if (!get_field('text') && qp_project_owner()) {
+        $text = __('Du kannst deinem Projekt eine Beschreibung hinzufügen, um die Ziele und Inhalte noch besser zu erklären.','quartiersplattform');
+        reminder_card('projekt-description-reminder'.get_the_ID(), __('Beschreibung hinzufügen','quartiersplattform'), $text);
+    }
 
+    if ( ( is_user_logged_in() && qp_project_owner() ) ) {
+        ?>
+            <a class="button is-style-outline" href="<?php qp_parameter_permalink('action=edit'); echo esc_url($link ); ?>"><?php _e('Beschreibung bearbeiten', 'quartiersplattform'); ?> </a>
+        <?php
+        }
 ?>
-
-<?php
-if ( ( is_user_logged_in() && qp_project_owner() ) ) {
-?>
-    <a class="button is-style-outline" href="<?php qp_parameter_permalink('action=edit'); echo esc_url($link ); ?>"><?php _e('Beschreibung bearbeiten', 'quartiersplattform'); ?> </a>
-<?php
-}
-?>
-
 
 <!-- Gutenberg Editor Content -->
 <div class="gutenberg-content">
-
     <?php
         if ( is_search() || ! is_singular() && 'summary' === get_theme_mod( 'blog_content', 'full' ) ) {
             the_excerpt();
